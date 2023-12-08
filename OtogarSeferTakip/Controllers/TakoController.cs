10d29960db6268846ca5c95e257f06f9a63a60de
bariskarabela <Kekre255013>
2023-12-08 19:13:49 +0300

    08.12.2023

--- OtogarSeferTakip/Controllers/TakoController.cs
+++ OtogarSeferTakip/Controllers/TakoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using OtogarSeferTakip.Entities;
+using OtogarSeferTakip.Models;
 
 namespace OtogarSeferTakip.Controllers
 {
@@ -14,22 +15,62 @@ namespace OtogarSeferTakip.Controllers
 
         public IActionResult Index()
         {
-           result = _databaseContext.();
+           var result = _databaseContext.Takos.ToList();
             return View(result);
         }
+        //public IActionResult Delete(int id)
+        //{
+        //    Tako tako = _databaseContext.Takos.Find(id);
+        //    if (tako != null)
+        //    {
+        //        _databaseContext.Remove(tako);
+        //        _databaseContext.SaveChanges();
+
+        //    }
+        //    return RedirectToAction(nameof(Index));
+        //}
         public IActionResult Add()
-        {
+        {         
             return View();
         }
+
         [HttpPost]
         public IActionResult Add(Tako tako)
         {
             if (ModelState.IsValid)
             {
-                _databaseContext.Takos.Add(tako);
+                _databaseContext.Takos.Add(tako);      
                 _databaseContext.SaveChanges();
+                TempData["Message"] = "Takograf Ekleme Başarılı.";
+
             }
-            return View(nameof(Add));
+            else
+            {
+                TempData["ErrorMessage"] = "Başarısız.";
+                ModelState.AddModelError(nameof(tako.TakoName), "Hata.");
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Edit(Tako id)
+        {
+            Tako tako = _databaseContext.Takos.Find(id);
+            return View(tako);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Tako tako, int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var existingTako = _databaseContext.Takos.Find(id);
+
+
+                _databaseContext.SaveChanges();
+                TempData["Message"] = "Takograf Düzenlendi.";
+            }
+
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
